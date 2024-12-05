e9b243dead49bb267d5b8e083ed017bd2b42b659
nowhere132 <canh.manh123@gmail.com>
2024-12-05 19:39:01 +0700

    [nowhere132] List heroes in a pretty way

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
-import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
-  imports: [UpperCasePipe],
+  imports: [FormsModule, NgFor, NgIf, UpperCasePipe],
   providers: [],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
