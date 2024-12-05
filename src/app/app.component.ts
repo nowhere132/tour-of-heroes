42415445da82854e434694e963f8e4ead8c39988
nowhere132 <canh.manh123@gmail.com>
2024-12-05 18:40:26 +0700

    [nowhere132] Change app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'Tour of Heroes';
 }
